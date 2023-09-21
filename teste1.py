dc9603e124e0cda1806d24798210bc1f81ad7486
pg7top <111295676+pg7top@users.noreply.github.com>
2023-09-20 21:01:43 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,17 @@
+
+
+
+a = 33
+b = "33"
+
+# b = int(b)
+
+if b > a:
+    print("b é maior que a")
+elif b == a:
+    print("a e b são iguais")
+else:
+    print("a é maior que b")
+
+    #SyntaxError: '(' was never closed
+    #TypeError: '>' not supported between instances of 'str' and 'int'
